7e23b271910e6ac70722c80dc6cdba7515099005
wbalconi <wbalconi@3arrows.us>
2023-10-24 11:17:54 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿var sortedList = new List<int>();
+int number;
+
+while (sortedList.Count < 5)
+{
+    System.Console.Write("Enter a unique number:");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (sortedList.Contains(number))
+    {
+        Console.WriteLine("Number is already in the list");
+    }
+    else
+    {
+        sortedList.Add(number);
+        Console.WriteLine("Number added to the list");
+    }
+}
+
+sortedList.Sort();
+Console.Write("Sorted list: ");
+Console.WriteLine(string.Join(", ", sortedList));
+ 
